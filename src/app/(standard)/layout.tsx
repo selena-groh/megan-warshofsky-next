543533d326d7beba01538c2e62b34e1d8a24b9f2
Selena Groh <selena.groh@gmail.com>
2024-12-15 15:22:03 -0500

    update chakra

--- src/app/(standard)/layout.tsx
+++ src/app/(standard)/layout.tsx
@@ -1,16 +1,13 @@
-import "server-only";
 import Header from "@/components/Header";
 import { Container, Flex } from "@chakra-ui/react";
-import { SkipNavContent, SkipNavLink } from "@chakra-ui/skip-nav";
+import "server-only";
 
 const Layout = ({ children }) => {
   return (
     <Flex minHeight="100vh" flexDirection="column">
       <main>
         <Container py={{ base: 8 }} px={4} maxWidth="1600px" margin="auto">
-          <SkipNavLink>Skip to content</SkipNavLink>
           <Header />
-          <SkipNavContent />
           {children}
         </Container>
       </main>
